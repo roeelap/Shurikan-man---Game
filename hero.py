6c36d0b23b8cacb8750f67d175efc786c9ac27cf
Roeelap <rlapushin@gmail.com>
2020-11-16 20:40:03 +0200

    start

--- hero.py
+++ hero.py
@@ -1,18 +1,18 @@
 import pygame
 
 
-# hey
-# hi
 # PLAYER_STANDING = pygame.image.load('./data/player images/standing.png')
 
 
 PLAYER_WALK_RIGHT = [pygame.image.load('./data/player-images/R1.png'), pygame.image.load('./data/player-images/R2.png'), pygame.image.load('./data/player-images/R3.png'),
-                     pygame.image.load('./data/player-images/R4.png'), pygame.image.load('./data/player-images/R5.png'), pygame.image.load('./data/player-images/R6.png'),
+                     pygame.image.load('./data/player-images/R4.png'), pygame.image.load(
+                         './data/player-images/R5.png'), pygame.image.load('./data/player-images/R6.png'),
                      pygame.image.load('./data/player-images/R7.png'), pygame.image.load('./data/player-images/R8.png'), pygame.image.load('./data/player-images/R9.png')]
 
 
 PLAYER_WALK_LEFT = [pygame.image.load('./data/player-images/L1.png'), pygame.image.load('./data/player-images/L2.png'), pygame.image.load('./data/player-images/L3.png'),
-                    pygame.image.load('./data/player-images/L4.png'), pygame.image.load('./data/player-images/L5.png'), pygame.image.load('./data/player-images/L6.png'),
+                    pygame.image.load('./data/player-images/L4.png'), pygame.image.load(
+                        './data/player-images/L5.png'), pygame.image.load('./data/player-images/L6.png'),
                     pygame.image.load('./data/player-images/L7.png'), pygame.image.load('./data/player-images/L8.png'), pygame.image.load('./data/player-images/L9.png')]
 
 
@@ -35,20 +35,22 @@ class Player:
     def draw(self, window):
         if self.walk_count + 1 >= 27:
             self.walk_count = 0
-        
+
         if not self.standing:
-            if self.left:  
-                window.blit(PLAYER_WALK_LEFT[self.walk_count // 3], (self.x, self.y))
-                self.walk_count += 1                          
+            if self.left:
+                window.blit(
+                    PLAYER_WALK_LEFT[self.walk_count // 3], (self.x, self.y))
+                self.walk_count += 1
             elif self.right:
-                window.blit(PLAYER_WALK_RIGHT[self.walk_count // 3], (self.x, self.y))
+                window.blit(
+                    PLAYER_WALK_RIGHT[self.walk_count // 3], (self.x, self.y))
                 self.walk_count += 1
         else:
             if self.left:
                 window.blit(PLAYER_WALK_LEFT[0], (self.x, self.y))
             else:
                 window.blit(PLAYER_WALK_RIGHT[0], (self.x, self.y))
-        
+
         self.hitbox = (self.x + 17, self.y + 11, 28, 53)
         # pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
 
