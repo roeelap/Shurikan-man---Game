4572b7fad105a466d8d29c3c809754f89841e031
Omrigo312 <omrigo312@gmail.com>
2020-11-18 17:01:01 +0200

    changed enemy auth_path

--- enemy.py
+++ enemy.py
@@ -12,9 +12,11 @@ class Enemy(object):
         self.start_path_x = x
         self.end_path_x = end_path_x
         self.walk_count = 0
+        self.walk_count_limit = len(walk_right_images) * 3
         self.speed = speed
         self.hitbox = (self.x + 20, self.y + 5, 31, 59)
         self.health = health
+        self.max_health = health
         self.alive = True
         self.walk_right_images = walk_right_images
         self.walk_left_images = walk_left_images
@@ -22,10 +24,10 @@ class Enemy(object):
     def draw(self, window):
         self.auto_path()
 
-        if self.walk_count + 1 >= 33:
+        if self.walk_count + 1 >= self.walk_count_limit:
             self.walk_count = 0
 
-        if self.speed < 0:
+        if self.speed > 0:
             window.blit(
                 self.walk_right_images[self.walk_count // 3], (self.x, self.y))
             self.walk_count += 1
@@ -39,24 +41,23 @@ class Enemy(object):
         pygame.draw.rect(window, COLORS['red'],
                          (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
         pygame.draw.rect(
-            window, COLORS['green'], (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (9 - self.health)), 10))
+            window, COLORS['green'], (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (self.max_health - self.health)), 10))
 
         self.hitbox = (self.x + 20, self.y + 5, 31, 59)
         # pygame.draw.rect(window, (255,0,0), self.hitbox,2)
 
     def auto_path(self):
-        if self.speed > 0:
-            if self.x - self.speed > self.end_path_x:
-                self.x -= self.speed
-            else:
-                self.speed = self.speed * -1
-                self.walk_count = 0
+        inbound_left = self.x + self.speed > self.end_path_x
+        inbound_right = self.x + self.speed < self.start_path_x
+        print(inbound_left, inbound_right)
+        if inbound_left and self.speed < 0 or inbound_right and self.speed > 0:
+            self.x += self.speed
         else:
-            if self.x + self.speed < self.start_path_x:
-                self.x -= self.speed
-            else:
-                self.speed = self.speed * -1
-                self.walk_count = 0
+            self.turn_around()
+
+    def turn_around(self):
+        self.speed *= -1
+        self.walk_count = 0
 
     def move(self, background_speed, direction):
         self.x += background_speed * direction
