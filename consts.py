28f310bcf4770ba3e71a855f66736426f02aefd7
Roeelap <rlapushin@gmail.com>
2020-11-21 17:15:23 +0200

    added shuriken consts

--- consts.py
+++ consts.py
@@ -23,6 +23,8 @@ MAX_SHURIKENS = 3
 SHURIKEN_TIMEOUT = 6
 SHURIKEN_IMAGE = pygame.image.load('./data/images/player/shuriken.png')
 SHURIKEN_RADIUS = 12
+SHURIKEN_STARTING_SLOPE = 10
+SHURIKEN_ENDING_SLOPE = -20
 
 # colors dict
 COLORS = {'black': (0, 0, 0), 'white': (255, 255, 255),
--- shuriken.py
+++ shuriken.py
@@ -1,4 +1,4 @@
-from consts import SHURIKEN_IMAGE, SCREEN_WIDTH, SHURIKEN_RADIUS
+from consts import SHURIKEN_IMAGE, SCREEN_WIDTH, SHURIKEN_RADIUS, SHURIKEN_STARTING_SLOPE, SHURIKEN_ENDING_SLOPE
 
 
 class Shuriken:
@@ -7,20 +7,20 @@ class Shuriken:
         self.y = y
         self.radius = SHURIKEN_RADIUS
         self.speed = speed
-        self.y_change = 10
+        self.slope = SHURIKEN_STARTING_SLOPE
 
     def draw(self, window):
         window.blit(SHURIKEN_IMAGE, (self.x, self.y))
 
     def is_in_screen(self):
-        if self.x < SCREEN_WIDTH and self.x > 0 and self.y_change != -20:
-            if self.y_change >= -20:
+        if self.x < SCREEN_WIDTH and self.x > 0 and self.slope != SHURIKEN_ENDING_SLOPE:
+            if self.slope >= SHURIKEN_ENDING_SLOPE:
                 self.x += self.speed
-                self.y -= int((self.y_change *
-                                    abs(self.y_change)) * 0.1)
-                self.y_change -= 1
+                self.y -= int((self.slope *
+                                    abs(self.slope)) * 0.1)
+                self.slope -= 1
             else:
-                self.y_change = 10
+                self.slope = SHURIKEN_STARTING_SLOPE
             return True
         else:
             return False
