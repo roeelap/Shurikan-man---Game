a183ba0539d9d1fe4371a311ac77da63f129e5eb
Omrigo312 <omrigo312@gmail.com>
2020-11-24 22:33:29 +0200

    enemies spawn at end of screen

--- enemy.py
+++ enemy.py
@@ -1,6 +1,5 @@
 import pygame
-from consts import BOTTOM_BORDER, COLORS, FPS, GOBLIN_PATH_TIMEOUT, GOBLIN_SPAWN_TIMEOUT, SOUNDS, TOP_BORDER
-from path import Path
+from consts import BOTTOM_BORDER, COLORS, GOBLIN_PATH_TIMEOUT, GOBLIN_SPAWN_TIMEOUT, SOUNDS, TOP_BORDER
 from random import choice
 from static_functions import draw_circle_alpha
 from operator import itemgetter
@@ -8,7 +7,7 @@ from operator import itemgetter
 
 class Enemy:
 
-    def __init__(self, x, y, width, height, path, speed, health, walk_right_images, walk_left_images):
+    def __init__(self, x, y, width, height, speed, health, walk_right_images, walk_left_images):
         self.x = x
         self.y = y
         self.width = width
@@ -17,8 +16,6 @@ class Enemy:
         self.walk_count_limit = len(walk_right_images) * 6
         self.speed = speed
         self.vertical_speed = self.speed / 2
-        self.path = path
-        self.path_limit = Path(self.path.start, self.path.end)
         self.max_speed = abs(speed)
         self.hitbox = (0, 0, 0, 0)
         self.health = health
@@ -108,9 +105,6 @@ class Enemy:
         pygame.draw.rect(window, COLORS['black'],
                          (x, y, w, h), width=1)
 
-    def update_path_limits(self, player_speed, direction):
-        self.path.start -= player_speed * direction
-        self.path.end -= player_speed * direction
 
     def horizontal_turn_around(self):
         self.speed *= -1
