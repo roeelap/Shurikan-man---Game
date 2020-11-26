8a7c768bf4fa7d470e7e1c7f60c3f2b30d008a95
Roeelap <rlapushin@gmail.com>
2020-11-26 21:09:49 +0200

    fixed level text in player stat

--- menu/player_stat.py
+++ menu/player_stat.py
@@ -12,8 +12,8 @@ class PlayerStat:
         self.level = upgrade_dict[self.name]
         self.level_delta = 0
         self.next_rect_y = self.y - 50
-        self.up_button = Button(self.x, self.y - ARROW_BUTTON_HEIGHT , 'up_arrow')
-        self.down_button = Button(self.x + ARROW_BUTTON_WIDTH + 5, self.y - ARROW_BUTTON_HEIGHT , 'down_arrow')
+        self.up_button = Button(self.x, self.y - ARROW_BUTTON_HEIGHT - 5 , 'up_arrow')
+        self.down_button = Button(self.x + ARROW_BUTTON_WIDTH + 5, self.y - ARROW_BUTTON_HEIGHT - 5, 'down_arrow')
         self.is_confirmed = True
         self.bar_timer = 0
     
@@ -24,12 +24,11 @@ class PlayerStat:
             self.level_delta += 1
 
     def downgrade_stat(self, player):
-        if self.level_delta > 0:
+        if self.level >= 0 and self.level_delta > 0:  
             self.level_delta -= 1
             player.upgrade_points += 1
-        else:
-            if self.level > 0:
-                self.level -= 1
+        elif self.level > 0 and self.level_delta == 0:
+            self.level -= 1
 
     def show(self, window, mouse, player):
         if self.bar_timer + 1 <= FPS:
@@ -37,15 +36,7 @@ class PlayerStat:
         else:
             self.bar_timer = 0
 
-        name = PIXEL_FONT_SMALL.render(str(self.name).replace(
-            '_', ' ').capitalize(), True,  COLORS['white'])
-        window.blit(name, (self.x, self.y))
-
-        if self.level == 20:   
-            level_text = PIXEL_FONT_SMALL.render(('Level: Max'), True,  COLORS['white'])
-        else:
-            level_text = PIXEL_FONT_SMALL.render(('Level: ' + str(self.level)), True,  COLORS['white'])
-        window.blit(level_text, (self.x, self.y - name.get_rect()[3] + ARROW_BUTTON_HEIGHT + 10))
+        self.show_name_and_level(window)
         
         if player.upgrade_points == 0 or self.level + self.level_delta == 20:
             self.up_button.disabled = True
@@ -61,14 +52,26 @@ class PlayerStat:
         self.down_button.show(window, mouse)
 
         for i in range(self.level):
-            pygame.draw.rect(window, (180, i * 9, i * 9), [self.x, self.y - 50 - i * 15, ARROW_BUTTON_WIDTH * 2, 15])
-            pygame.draw.rect(window, COLORS['black'], [self.x, self.y - 50 - i * 15, ARROW_BUTTON_WIDTH * 2, 15], width=1)
+            pygame.draw.rect(window, (180, i * 9, i * 9), [self.x, self.up_button.y - 20 - i * 15, ARROW_BUTTON_WIDTH * 2, 15])
+            pygame.draw.rect(window, COLORS['black'], [self.x, self.up_button.y - 20 - i * 15, ARROW_BUTTON_WIDTH * 2, 15], width=1)
 
         for i in range(self.level_delta):
             if self.bar_timer <= FPS * 4 / 5:
-                draw_rect_with_alpha(self.x, self.y - 50 - (self.level + i) * 15, ARROW_BUTTON_WIDTH * 2, 15, (180, i * 9, i * 9), 128, window)
-                pygame.draw.rect(window, COLORS['black'], [self.x, self.y - 50 - (self.level + i) * 15, ARROW_BUTTON_WIDTH * 2, 15], width=1)
+                draw_rect_with_alpha(self.x, self.up_button.y - 20 - (self.level + i) * 15, ARROW_BUTTON_WIDTH * 2, 15, (180, i * 9, i * 9), 128, window)
+                pygame.draw.rect(window, COLORS['black'], [self.x, self.up_button.y - 20 - (self.level + i) * 15, ARROW_BUTTON_WIDTH * 2, 15], width=1)
 
         if self.is_confirmed:
             self.level += self.level_delta
             self.level_delta = 0
+
+    def show_name_and_level(self, window):
+        name = PIXEL_FONT_SMALL.render(str(self.name).replace(
+            '_', ' ').capitalize(), True,  COLORS['white'])
+        window.blit(name, (self.x, self.y))
+
+        if self.level + self.level_delta == 20:   
+            level_text = PIXEL_FONT_SMALL.render(('Level: Max'), True,  COLORS['white'])
+        else:
+            level_text = PIXEL_FONT_SMALL.render(('Level: ' + str(self.level + self.level_delta)), True,  COLORS['white'])
+        window.blit(level_text, (self.x, self.y - name.get_rect()[3] + ARROW_BUTTON_HEIGHT + 10))
+    
