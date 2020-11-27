63f5ad20e32169ef875485884ac83aef83c42121
Roeelap <rlapushin@gmail.com>
2020-11-27 21:37:53 +0200

    xp bar bug

--- player.py
+++ player.py
@@ -9,30 +9,38 @@ class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
         self.width = 77
         self.height = 77
+
         self.speed = 2.5
         self.left = False
         self.right = False
         self.standing = True
         self.walk_count = 3
+
         self.image = PLAYER_STANDING_IMAGE
         self.hitbox = (0, 0, 0, 0)
+        self.shade = {'x': 0, 'y': 0, 'w': 0, 'h': 0}
+
         self.max_health = 10
         self.health = self.max_health
         self.hurt_counter = 0
+
         self.xp = 0
         self.level = 1
         self.xp_to_next_lvl = self.level ** 2 * 10
+
         self.upgrade_points = 10
-        self.score = 0
-        self.coins = 0
-        self.throw_speed = 12
-        self.shade = {'x': 0, 'y': 0, 'w': 0, 'h': 0}
-        self.shurikens_owned = ['shuriken']
-        self.shuriken_equipped = 'shuriken'
         self.upgrades = {'max_health': 0, 'speed': 0, 'strength': 0,
                          'throw_speed': 0, 'max_shurikens': 0, 'reload speed': 0, 'group damage': 0}
+
+        self.score = 0
+        self.coins = 0
+
+        self.throw_speed = 12
+        self.shurikens_owned = ['shuriken']
+        self.shuriken_equipped = 'shuriken'
 
     def draw(self, window):
         if self.walk_count + 1 >= 54:
@@ -102,48 +110,14 @@ class Player:
         self.hurt_counter -= 1
 
     def display_player_stats(self, window):
-        health_bar_x = SCREEN_WIDTH * 2 // 20
-        health_bar_width = SCREEN_WIDTH * 17 // 20
-
-        pygame.draw.rect(
-            window, COLORS['red'], (health_bar_x, 30, health_bar_width, 30), border_radius=15)
-        pygame.draw.rect(
-            window, COLORS['green'], (health_bar_x, 30, (health_bar_width) - ((health_bar_width // 10) * (self.max_health - self.health)), 30), border_radius=15)
-        pygame.draw.rect(window, COLORS['black'], (SCREEN_WIDTH *
-                                                   2 // 20, 30, health_bar_width, 30), width=3, border_radius=15)
-        if self.xp > 0:
-            pygame.draw.rect(window, COLORS['orange'], (health_bar_x, 60, (
-                health_bar_width) * (self.xp / self.xp_to_next_lvl), 20), border_radius=15)
-        pygame.draw.rect(window, COLORS['black'], (SCREEN_WIDTH *
-                                                   2 // 20, 60, health_bar_width, 20), width=2, border_radius=15)
-        for i in range(1, 4):
-            pygame.draw.line(window, COLORS['black'], (health_bar_x + (
-                health_bar_width * i / 4), 60), (health_bar_x + (health_bar_width * i / 4), 80), width=2)
-
+        self.display_health_bar(window)
+        self.display_xp_bar(window)
+        self.display_xp_text(window)
         window.blit(PLAYER_PORTRAIT, (SCREEN_WIDTH // 30, 30))
-
-        health = f'{self.health} / {self.max_health}'
-        if self.health == 0:
-            health = 'DEAD'
-        health_text = PIXEL_FONT_SMALL.render(health, True,  COLORS['white'])
-        window.blit(health_text, (130, 35))
-
-        xp = '% ' + str(round(self.xp / self.xp_to_next_lvl * 100, 2))
-        xp_text = PIXEL_FONT_SMALL.render(xp, True,  COLORS['white'])
-        window.blit(xp_text, (130, 61))
-
-        level = f'Level: {self.level}'
-        level_text = PIXEL_FONT_SMALL.render(level, True,  COLORS['white'])
-        window.blit(level_text, (health_bar_x + health_bar_width -
-                                 level_text.get_rect()[2] - 10, 61))
-
-        score = f'Score: {self.score}'
-        score_text = PIXEL_FONT_SMALL.render(score, True,  COLORS['white'])
-        window.blit(score_text, (25, 80))
-
-        coins = f'Coins: {self.coins}'
-        coins_text = PIXEL_FONT_SMALL.render(coins, True,  COLORS['white'])
-        window.blit(coins_text, (25, 100))
+        self.display_health_text(window)
+        self.display_level_text(window)
+        self.display_score_text(window)
+        self.display_coins_text(window)
 
     def level_up(self):
         SOUNDS['level_up'].play()
@@ -156,3 +130,62 @@ class Player:
             self.xp -= self.xp_to_next_lvl
             self.level_up()
             self.xp_to_next_lvl = self.level ** 2 * 10
+
+    def display_health_bar(self, window):
+        health_bar_x = SCREEN_WIDTH * 2 // 20
+        health_bar_width = SCREEN_WIDTH * 17 // 20
+        pygame.draw.rect(
+            window, COLORS['red'], (health_bar_x, 30, health_bar_width, 30), border_radius=15)
+        pygame.draw.rect(
+            window, COLORS['green'], (health_bar_x, 30, (health_bar_width) - ((health_bar_width // 10) * (self.max_health - self.health)), 30), border_radius=15)
+        pygame.draw.rect(window, COLORS['black'], (SCREEN_WIDTH *
+                                                   2 // 20, 30, health_bar_width, 30), width=3, border_radius=15)
+
+    def display_xp_bar(self, window):
+        health_bar_x = SCREEN_WIDTH * 2 // 20
+        health_bar_width = SCREEN_WIDTH * 17 // 20
+        xp_owned_width = (health_bar_width) * (self.xp / self.xp_to_next_lvl)
+        border_radius = 15
+
+        # if xp_owned_width < border_radius:
+        #     border_radius = int(xp_owned_width)
+        if self.xp > 0:
+            pygame.draw.rect(window, COLORS['orange'], (health_bar_x + abs(xp_owned_width - border_radius), 60, xp_owned_width, 20), border_radius=20)
+        pygame.draw.rect(window, COLORS['black'], (SCREEN_WIDTH *
+                                                   2 // 20, 60, health_bar_width, 20), width=2, border_radius=15)
+        for i in range(1, 4):
+            pygame.draw.line(window, COLORS['black'], (health_bar_x + (
+                health_bar_width * i / 4), 60), (health_bar_x + (health_bar_width * i / 4), 80), width=2)
+        print((
+            health_bar_width) * (self.xp / self.xp_to_next_lvl))
+
+    def display_coins_text(self, window):
+        coins = f'Coins: {self.coins}'
+        coins_text = PIXEL_FONT_SMALL.render(coins, True,  COLORS['white'])
+        window.blit(coins_text, (25, 100))
+
+    def display_score_text(self, window):
+        score = f'Score: {self.score}'
+        score_text = PIXEL_FONT_SMALL.render(score, True,  COLORS['white'])
+        window.blit(score_text, (25, 80))
+
+    def display_level_text(self, window):
+        health_bar_x = SCREEN_WIDTH * 2 // 20
+        health_bar_width = SCREEN_WIDTH * 17 // 20
+
+        level = f'Level: {self.level}'
+        level_text = PIXEL_FONT_SMALL.render(level, True,  COLORS['white'])
+        window.blit(level_text, (health_bar_x + health_bar_width -
+                                 level_text.get_rect()[2] - 10, 61))
+
+    def display_xp_text(self, window):
+        xp = '% ' + str(round(self.xp / self.xp_to_next_lvl * 100, 2))
+        xp_text = PIXEL_FONT_SMALL.render(xp, True,  COLORS['white'])
+        window.blit(xp_text, (130, 61))
+
+    def display_health_text(self, window):
+        health = f'{self.health} / {self.max_health}'
+        if self.health == 0:
+            health = 'DEAD'
+        health_text = PIXEL_FONT_SMALL.render(health, True,  COLORS['white'])
+        window.blit(health_text, (130, 35))
